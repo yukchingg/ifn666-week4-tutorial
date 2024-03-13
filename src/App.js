d5c2d512438cbdbbe317863d3a12cb90a8911ba7
yukchingg <wychan636@hotmail.com>
2024-03-13 15:54:28 +1000

    week3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import logo from './QUT-logo.png';
 import './App.css';
+
+const animals= [
+  {
+    name: "Lion",
+    number: 3,
+    eats: ["zebra", "antelope", "buffalo", "hippopotamus"],
+  },
+  {
+    name: "Tiger",
+    number: 5,
+    eats: ["moose", "deer", "buffalo"],
+  },
+  {
+    name: "Giraffe",
+    number: 6,
+    eats: ["leaves", "twigs"],
+  },
+  {
+    name: "Elephant",
+    number: 4,
+    eats: ["grass", "leaves", "flowers", "fruit"],
+  },
+  {
+    name: "Monkey",
+    number: 10,
+    eats: ["fruit", "leaves", "vegetables", "insects"],
+  },
+  {
+    name: "Lemur",
+    number: 15,
+    eats: ["fruit", "leaves", "insects"],
+  },
+  {
+    name: "Rhinoceros",
+    number: 2,
+    eats: ["grass", "shoots", "leaves", "berries"],
+  },
+];
+
+function AnimalComponent({name, number, eats}) {
+  return (
+      <div >
+        
+          <h1 > { name } </h1>
+          <p > { number } </p>
+          <p > { eats.join(", ") } </p>
+      </div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <h1>QUT Zoo </h1>
+
+        <div>
+        {
+          animals.map(animal => (
+          <AnimalComponent {...animal}/>
+        ))
+        }
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.qut.edu.au"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit QUT
         </a>
+
       </header>
+
     </div>
   );
 }
-
+  
 export default App;
